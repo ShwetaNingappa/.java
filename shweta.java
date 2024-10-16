d636e0e261fe18bbc591bb73632a9a9be91767c5
Shweta <shwetaningappa2004@gmail.com>
2024-10-16 12:39:12 +0530

    Create shweta.java

---
+++ shweta.java
@@ -0,0 +1,18 @@
+public class PrimeCheck {
+    public static void main(String[] args) {
+        int num = 29; // Example number to check
+        boolean isPrime = true;
+
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if(num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if(isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
